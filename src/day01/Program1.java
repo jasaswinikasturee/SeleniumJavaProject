ede7006f755b1df5f9a46ee11a9748eafbc618c2
jasaswinikasturee <132183556+jasaswinikasturee@users.noreply.github.com>
2023-06-14 08:03:01 +0530

    Initial commit

---
+++ src/day01/Program1.java
@@ -0,0 +1,16 @@
+package day01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com");
+	}
+
+}
